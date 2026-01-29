939a69aa171de247e71cbc7920138ffa7587dfb8
siyatut <anastasiya.tyutinova@gmail.com>
2026-01-29 12:16:39 +0300

    split index.css into modules

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
 import { ShopProvider } from "./store/shop";
+
+import "./styles/base.css";
+import "./styles/layout.css";
+import "./styles/header.css";
+import "./styles/card.css";
+import "./styles/product-modal.css";
+import "./styles/cart.css";
+import "./styles/checkout-modal.css";
+import "./styles/footer.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
